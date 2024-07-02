06b8298f77911dbad154917d12c22e431ae0be81
vdviviana <vdviviana@hotmail.com>
2024-07-01 22:14:25 -0300

    Vista devuelve json

--- app/views.py
+++ app/views.py
@@ -1,1 +1,11 @@
 #define las rutas y la logica de negocio de la aplicacion
+
+# jsonify define json a partir de objetos python
+from flask import jsonify
+
+def index():
+
+    #devuelve json de datos como api
+    return jsonify({
+        'mensaje': 'Hello World APIS con FLASK'})
+    #las llaves hablan de diccionario
