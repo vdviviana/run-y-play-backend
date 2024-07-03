dd9406325b8074e4752dba20f7b36ee6090d32a0
vdviviana <vdviviana@hotmail.com>
2024-07-03 13:20:34 -0300

    Agrego rutas de funciones en la vista

--- run.py
+++ run.py
@@ -4,9 +4,22 @@ from app.database import test_connection
 
 app =Flask(__name__)
 
-#rutas de mi Api REst
+# ruta funcion de la vista -> devuelve mensaje en json
 app.route('/', methods=['GET'])(index)
 
+# ruta funcion de la vista -> consulta usuarios
+app.route('/api/users/all', methods=['GET'])(get_users)
+
+# ruta de funcion de la vista -> consulta user por id
+app.route('/api/users/byid/<int:users_id>', methods=['GET'])(get_users_byid)
+
+# ruta de funcion de la vista -> crea usuario mediante post
+app.route('/api/users/create/', methods=['POST'])(create_new_user)
+
+# ruta de funcion de la vista -> actualiza usuario mediante post
+app.route('/api/users/update/<int:users_id>', methods=['PUT'])(update_users_byid)
+
+# funcion de prueba de conexion a la base de datos
 test_connection()
 
 if __name__=='__main__':
